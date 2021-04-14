36bce97f4fc75ee3658602652a2f89afe44ac4fa
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2021-04-14 07:54:25 -0700

    Create 200_number_of_islands.cpp

---
+++ 200_number_of_islands.cpp
@@ -0,0 +1,52 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int H=grid.length();
+        if(!H)return 0;
+        
+        int W=grid[0].size();
+        
+        if(!W) return 0;
+        
+        int count=0;
+        
+        
+        for(int i=0;i<H;i++){
+            for(int j=0;j<W;j++){
+                if(grid[i][j]=='1'){
+                    count++;
+                    dfs(grid,i,j);
+                }
+            }
+        }
+        return count;
+    }
+    
+     void dfs(vector<vector<char>>& grid, int i, int j){
+          int H=grid.size();
+     
+        
+          int W=grid[0].size();
+          grid[i][j]='0';
+         
+         if(i+1 < H && grid[i+1][j]=='1'){
+             dfs(grid,i+1,j);
+         }
+                 
+         if(j+1 < W && grid[i][j+1] =='1'){
+             dfs(grid,i,j+1);
+         }
+                 
+                 
+        if(i-1 >=0 && grid[i-1][j] =='1'){
+             dfs(grid,i-1,j);
+        }
+            
+                  
+        if(j-1 >=0 && grid[i][j-1] =='1'){
+             dfs(grid,i,j-1);
+                 
+         }
+    
+     }  
+    }
+}
