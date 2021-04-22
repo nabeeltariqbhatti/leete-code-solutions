a5b316b3d50c6f4bdc4cf0470715c869e0868b40
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2021-04-22 12:59:28 +0500

    Update cpp_containers_iterators_beginand_end.cpp
    Vector methods

--- cpp_containers_iterators_beginand_end.cpp
+++ cpp_containers_iterators_beginand_end.cpp
@@ -80,3 +80,62 @@ int main()
     return 0;
 }
 
+
+
+/******************************************************************************
+
+Welcome to GDB Online.  vector methods
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include<vector>
+
+using namespace std;
+int main()
+{
+   vector<int > v={1,2,3,4,5,6,7,8} ;
+   
+   
+  cout<<"v.size() => "<< " "<< v.size()<<endl;
+  
+  cout<<"v.capacity() => "<< " "<< v.capacity()<<endl;
+  
+  cout<<"v.max_size() => "<< " "<< v.max_size()<<endl;
+  
+  cout<<"v.puh_back(200)"<<endl;
+  v.push_back(200);
+  
+  
+  for(int n : v){
+      cout<<n<<endl;
+  }
+  
+  cout<<"v.back()"<< " "<<v.back();
+  v.pop_back();
+  cout<<"v.pop_back()"<<endl;
+   for(int n : v){
+      cout<<n<<endl;
+  }
+  
+  cout<<v[4]<<endl;
+  
+  
+  cout<<v.at(4)<<endl;
+  
+  cout<<"v.front()"<< " "<<v.front()<<endl;
+  
+  v.erase(v.begin()+1,v.begin()+2);
+  cout<<"v.erase(optional index)"<<endl;
+
+  cout<<"v.pop_back()"<<endl;
+   for(int n : v){
+      cout<<n<<endl;
+  }
+  
+    return 0;
+}
+
+
