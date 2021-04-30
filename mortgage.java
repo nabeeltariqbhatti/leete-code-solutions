89deeafa1c7097e3a084a059cd3dbf55bbecd18e
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2021-04-30 10:44:47 -0700

    Update mortgage.java

--- mortgage.java
+++ mortgage.java
@@ -1,6 +1,6 @@
-
 package mortgageCalculator;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class MortgageCalculator {
@@ -8,14 +8,22 @@ public class MortgageCalculator {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in); 
       	System.out.print("Enter Principal Amount : ");
-      
-      	System.out.print("Enter Rate of Interest : ");
-    
-    
+        Integer principal=scan.nextInt();
+      	System.out.print("Enter Annual Rate of Interest : ");
+      	double rate=scan.nextDouble();
+      	
+      	rate=(rate/100)/12;
+      	
       	System.out.print("Enter Time period in years : ");
+      	float period=scan.nextFloat();
+      	
+      	period=period*12;
+      	
+      	float mortgage= (float) (principal*(rate*(Math.pow(1+rate, period)/((Math.pow(1+rate, period)-1)))));
       
-      	//
-      	System.out.println("Payment: " );
+      	
+      	System.out.println("Payment: "+NumberFormat.getCurrencyInstance().format(mortgage) );
+      	
 	}
 
 }
