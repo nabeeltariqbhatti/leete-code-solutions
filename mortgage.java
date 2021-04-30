dd05ecbde9ba18fae7e8f3a682a929b102cccc65
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2021-04-30 10:30:18 -0700

    Update mortgage.java

--- mortgage.java
+++ mortgage.java
@@ -1,1 +1,21 @@
 
+package mortgageCalculator;
+
+import java.util.Scanner;
+
+public class MortgageCalculator {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in); 
+      	System.out.print("Enter Principal Amount : ");
+      
+      	System.out.print("Enter Rate of Interest : ");
+    
+    
+      	System.out.print("Enter Time period in years : ");
+      
+      	//
+      	System.out.println("Payment: " );
+	}
+
+}
