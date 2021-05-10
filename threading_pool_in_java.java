eb3322138f84c97ec5628ba82a786612c9e1369a
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2021-05-10 07:52:41 -0700

    Create threading_pool_in_java.java

---
+++ threading_pool_in_java.java
@@ -0,0 +1,55 @@
+package bhatti.com.example;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class Handler implements Runnable{
+	
+	private int id;
+	
+	public Handler(int id){
+		this.id=id;	
+		
+	}
+	
+	
+
+	@Override
+	public void run() {
+		System.out.println("Starting : " + id);
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Ending : " + id);
+		
+		
+	}
+	
+}
+
+public class Processor {
+
+	public static void main(String[] args) {
+		ExecutorService executor= Executors.newFixedThreadPool(2);
+		
+		for (int i=0; i<5; i++) {
+			executor.submit(new Handler(i));
+		}
+		executor.shutdown();
+		System.out.println("All Tasks are Executed...");
+		
+		try {
+			executor.awaitTermination(1, TimeUnit.DAYS);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
