b229730c178735e867c4f1d0fa180e1ce5231e2d
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2021-07-30 14:24:38 +0500

    Create 153-find-minimum-in-sorted-array.java

---
+++ 153-find-minimum-in-sorted-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int l = 0, r = nums.length-1;
+        while(l < r){
+            int mid = l + (r-l)/2;
+            if(nums[mid] > nums[r]) l = mid+1;
+            else if(nums[mid] < nums[r]) r = mid;
+            else return nums[r];
+        }
+        return nums[l];
+    }
+}
