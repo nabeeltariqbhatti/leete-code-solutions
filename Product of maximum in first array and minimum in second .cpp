df7f815ffe918e5c1acadcebf2577c677817b23c
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2021-08-20 08:32:43 -0700

    Create Product of maximum in first array and minimum in second .cpp

---
+++ Product of maximum in first array and minimum in second .cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+        long long find_multiplication(int a[], int b[], int n, int m)
+    {
+        int min = *std::max_element(a,a+n);
+        long long int max = *std::min_element(b,b+m);
+        
+        return max*min;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)    cin>>a[i];
+        
+        int m;
+        cin>>m;
+        int b[m];
+        for(int i=0;i<m;i++)    cin>>b[i];
+        
+        
+        Solution ob;
+        cout<<ob.find_multiplication(a, b, n, m)<<endl;
+
+    }
+    return 0;
+}  // } Driver Code Ends
